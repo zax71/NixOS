f68897555d233821acf5ee40c39d5a5bd9a70ae6
Zax71 <67716263+zax71@users.noreply.github.com>
2024-12-04 15:16:18 +0000

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "System config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
