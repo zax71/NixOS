aff07d3a18cf64bc874c7356e60694fb483696cc
Zax71 <67716263+zax71@users.noreply.github.com>
2025-10-24 15:54:52 +0100

    Fix virtualisation

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -5,6 +5,7 @@
   users.groups.libvirtd.members = [ "zax" ];
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.enable = true;
 
   virtualisation.spiceUSBRedirection.enable = true;
 }
