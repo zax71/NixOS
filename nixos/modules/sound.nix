00db1716596de6ed1e1c0fb45d3facd829d1dadf
Zax71 <67716263+zax71@users.noreply.github.com>
2025-12-27 13:29:36 +0000

    Mess with Pipewire audio stuff

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
-  # hardware.pulseaudio.enable = true;
+  # rtkit (optional, recommended) allows Pipewire to use the realtime scheduler for increased performance.
+  security.rtkit.enable = true;
   boot.extraModprobeConfig = ''
     options snd_hda_intel enable=0,1
   '';
