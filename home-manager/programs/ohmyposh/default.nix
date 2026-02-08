cecefd7a9ecce5522d6364042c7f9623001ce9c7
Zax71 <67716263+zax71@users.noreply.github.com>
2026-02-08 17:56:17 +0000

    Update Nixpkgs & Fix Oh My Posh

--- home-manager/programs/ohmyposh/default.nix
+++ home-manager/programs/ohmyposh/default.nix
@@ -2,8 +2,6 @@
   programs.oh-my-posh = {
     enable = true;
     enableZshIntegration = true;
-    settings = builtins.fromTOML (
-      builtins.unsafeDiscardStringContext (builtins.readFile ./config.toml)
-    );
+    configFile = ./oh-my-posh.toml;
   };
 }
