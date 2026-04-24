a2dce248fb667b5cc17cd1fbad05a80557c35f66
Zax71 <67716263+zax71@users.noreply.github.com>
2026-04-24 17:39:52 +0100

    Change the base packages to dendritic

---
+++ modules/hosts/z-pc/graphics.nix
@@ -0,0 +1,24 @@
+{ self, inputs, ... }:
+{
+
+  flake.nixosModules.zPcGraphics =
+    { pkgs, ... }:
+
+    {
+      hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          rocmPackages.clr
+          rocmPackages.clr.icd
+          rocmPackages.rocm-runtime
+        ];
+      };
+
+      environment.systemPackages = with pkgs; [
+        rocmPackages.rocm-smi
+        rocmPackages.rocm-device-libs
+        rocmPackages.hipcc
+      ];
+      services.xserver.videoDrivers = [ "amdgpu" ];
+    };
+}
